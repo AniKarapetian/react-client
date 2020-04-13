9aee61d621dc91b0937665bbaa8289a696245aac
Ani <ani-karapetyan00@list.ru>
2020-04-13 21:27:58 +0400

    Updates

---
+++ precache-manifest.5766a542f9db1734ff49f58f7dd3d9a6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7933e2d2910502a5193680ac7c9c423b",
+    "url": "/react-client/index.html"
+  },
+  {
+    "revision": "e1d7f32a8eb86227c39c",
+    "url": "/react-client/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6429440b8075986c3d54",
+    "url": "/react-client/static/css/main.0981ae59.chunk.css"
+  },
+  {
+    "revision": "e1d7f32a8eb86227c39c",
+    "url": "/react-client/static/js/2.36dc02ed.chunk.js"
+  },
+  {
+    "revision": "4fe3295419de00405cacb1c50c6956d6",
+    "url": "/react-client/static/js/2.36dc02ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6429440b8075986c3d54",
+    "url": "/react-client/static/js/main.ed986ddb.chunk.js"
+  },
+  {
+    "revision": "93a0df9465093d5be7b7",
+    "url": "/react-client/static/js/runtime-main.3387bfbd.js"
+  },
+  {
+    "revision": "d0ed0a5ce30b7b325372c8d0c6e8dfb9",
+    "url": "/react-client/static/media/Preloader.d0ed0a5c.gif"
+  },
+  {
+    "revision": "4663dc9c4df6c957e554b9bf2e5d38f6",
+    "url": "/react-client/static/media/delete_icon.4663dc9c.png"
+  },
+  {
+    "revision": "46eccaaba0e926af835b2ed22f9cb158",
+    "url": "/react-client/static/media/edit_icon.46eccaab.png"
+  },
+  {
+    "revision": "ba8c162b826277d555d7229b273f2914",
+    "url": "/react-client/static/media/home_logo.ba8c162b.png"
+  },
+  {
+    "revision": "6528a866730b84cffd840c737ba409f5",
+    "url": "/react-client/static/media/info_icon.6528a866.png"
+  },
+  {
+    "revision": "e473067656a6863aa110c2ebca745836",
+    "url": "/react-client/static/media/pageNotFound.e4730676.jpg"
+  },
+  {
+    "revision": "864ff03013a4ce7c6114315b05299113",
+    "url": "/react-client/static/media/user_photo.864ff030.png"
+  }
+]);
